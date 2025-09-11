882e099685ef4a319cac18e2a800f59913dd52f2
anhkhoa13_dev <itcsiu22278@student.hcmiu.edu.vn>
2025-09-11 09:10:51 +0700

    finish reroll

---
+++ Assets/Scripts/GamePlayScripts/Shop/ShopManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ShopManager : MonoBehaviour
+{
+    [SerializeField] private List<ShopItemData> items;
+
+    [SerializeField] private GameObject itemUIPrefab;
+
+    [SerializeField] private GameObject grid;
+
+    [SerializeField] private Button rollButton;
+
+    private void Awake()
+    {
+        rollButton.onClick.AddListener(RollItems);
+    }
+
+
+    void Start()
+    {
+        RollItems();
+    }
+
+    public void OpenShop()
+    {
+        gameObject.SetActive(true);
+    }
+
+
+    public void CloseShop()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void RollItems()
+    {
+        Debug.Log("Rolling new items...");
+
+        // Clear existing items
+        foreach (Transform child in grid.transform)
+        {
+            Destroy(child.gameObject);
+        }
+ 
+        foreach (var item in items.GetRandomElements(6))
+        {
+            GameObject obj = Instantiate(itemUIPrefab, grid.transform);
+            ShopItemUI ui = obj.GetComponent<ShopItemUI>();
+            ui.SetData(item);
+        }   
+    }
+
+}
