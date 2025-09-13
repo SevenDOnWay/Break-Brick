08c93cb1c4ddd66cf433cd37496696013af5ff5d
Seven <ngodaloc2006@gmail.com>
2025-09-13 22:14:06 +0700

    adding temp player data

---
+++ Assets/Scripts/PlayerData/PlayerData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerData {
+    // Player data class to hold player-related information
+
+    bool isLastGameEnded = false; // Check if player log out mid game last time 
+
+}
+ 
